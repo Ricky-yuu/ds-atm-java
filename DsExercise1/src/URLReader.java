981b1543b1347c2aaad82093dde4d91fc3b9d21d
Achmad Dinofaldi Firmansyah <47439247+Bangik@users.noreply.github.com>
2023-03-16 00:23:06 +0800

    url reader ds

--- DsExercise1/src/URLReader.java
+++ DsExercise1/src/URLReader.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class URLReader {
   public static void main(String[] args) throws Exception {
-    URL url = new URL("https://www.example.com/");
+    URL url = new URL("https://www.google.com/");
     BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
     in.lines().forEach(System.out::println);
     in.close();
